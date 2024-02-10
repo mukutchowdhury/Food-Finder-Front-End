4899bb6628e282c085f02f1fa0b4be8b87861f9b
jw6680 <jw6680@nyu.edu>
2024-02-09 23:38:13 -0500

    created navbar for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import './App.css'
+
+import Navbar from './Components/Navbar';
 
 function App() {
 
   return (
-    <>
-      <h1>Fooder-Finder FrontEnd</h1>
-    </>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+       <Route path = "" element = {<h1>home page</h1>} />
+        <Route path = "login" element = {<h1>Login/Sign up page</h1>} />
+        <Route path = "home" element = {<h1>home page</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PAGES = [
+    { label: 'Home', destination: '/' },
+    { label: 'Login', destination: '/login' },
+    { label: 'Restaruants', destination: '/restaurants' },
+  ];
+
+function Navbar() {
+
+    const mapper = (page) => (
+    <li>
+        <Link to={page.destination}>
+            {page.label}
+        </Link>
+    </li>
+    );
+
+    return (
+        <nav>
+            <ul>
+                {PAGES.map(mapper)} 
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
