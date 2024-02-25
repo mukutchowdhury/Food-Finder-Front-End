9dd8bb2221d38d259832256d639a8bdcfd33f233
Mukut <mc8100@nyu.edu>
2024-02-24 22:38:40 -0500

    Added changes in Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-import HomeView from './views/home'
+//import HomeView from './views/home'
 //import LoginView from './views/login'
-//import SignUpView from './views/signupStyle'
+import SignUpView from './views/signupStyle'
 
 import './index.css'
 
@@ -11,6 +11,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomeView/>
+    <SignUpView/>
   </React.StrictMode>,
 )
