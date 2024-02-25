698a65863b9b7ef93d45f965ca15ec4ac0c33d00
afanc0 <gianromani.gr@gmail.com>
2024-02-25 15:05:24 -0500

    temp render app view

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
-import HomeView from './views/home'
+import App from './App.jsx'
+//import HomeView from './views/home'
 //import LoginView from './views/login'
 //import SignUpView from './views/signupStyle'
 
@@ -11,6 +11,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomeView/>
+    <App/>
   </React.StrictMode>,
 )
