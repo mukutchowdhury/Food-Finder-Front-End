273cde2838e179cd6d498b3cbf0e7ffdb11a9a86
jw6680 <jw6680@nyu.edu>
2024-02-24 23:00:32 -0500

    updated correct path

--- cloud.sh
+++ cloud.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 #run frontend against local API server
-export REACT_APP_URL_PRE="http://mongodb+srv://jw6680:softpwd6680"
-                        + "@food-finder.ltqe7ym.mongodb.net/"
-                        + "?retryWrites=true&w=majority&appName=Food-Finder/"
+export REACT_APP_URL_PRE="http://foodfinder.pythonanywhere.com/"
 npm run dev
