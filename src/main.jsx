3baa5020227d9162f49916f4e5750c42e650b0a1
afanc0 <gianromani.gr@gmail.com>
2024-02-27 13:05:31 -0500

    added routing function

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import HomeView from './views/home'
 //import LoginView from './views/login'
 //import SignUpView from './views/signupStyle'
+import Review from './Components/Review.jsx'
 
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
-
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route>
+      <Route index element={<App />} />
+      <Route path='/review/:id' element={<Review />} />
+  </Route>
+));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
