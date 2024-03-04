c9e5717c6b471274a20c91580c7846ce41404e7c
afanc0 <gianromani.gr@gmail.com>
2024-03-04 15:42:45 -0500

    added callback prop

--- src/Components/IconFilterCard.jsx
+++ src/Components/IconFilterCard.jsx
@@ -2,12 +2,16 @@ import PropTypes from 'prop-types'
 
 function IconFilterCard(props) {
 
-    const {filterName, filterImage} = props;
+    const {filterName, filterImage, callback} = props;
 
+    const handleClickAction = () => {
+        callback(filterName);
+        console.log(filterName)
+    }
     return (
         <div className='ml-2 pl-[2px] snap-start relative max-w-full p-0'> 
             <div>
-                <div className='flex items-center justify-center flex-col gap-1 cursor-pointer p-1 rounded-2xl w-auto whitespace-nowrap'>
+                <div className='flex items-center justify-center flex-col gap-1 cursor-pointer p-1 rounded-2xl w-auto whitespace-nowrap' onClick={handleClickAction}>
                     <div className='w-[60px] h-[60px] overflow-hidden block mx-auto'>
                         <img className='block w-full h-full object-cover box-border' src={filterImage} style={{ objectPosition: '50% 50%' }} ></img>
                     </div>
@@ -21,6 +25,7 @@ function IconFilterCard(props) {
 IconFilterCard.propTypes = {
     filterName: PropTypes.string.isRequired,
     filterImage: PropTypes.object.isRequired,
+    callback: PropTypes.object.isRequired,
 };
 
 export default IconFilterCard;
