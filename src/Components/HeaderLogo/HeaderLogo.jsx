ae7c2a2b4662449845498422c9ae7a02ff1ef742
afanc0 <gianromani.gr@gmail.com>
2024-03-09 18:06:52 -0500

    created header component

---
+++ src/Components/HeaderLogo/HeaderLogo.jsx
@@ -0,0 +1,10 @@
+
+export default function HeaderLogo() {
+    return (
+        <div className='flex justify-center items-center h-12 sticky t-0 z-[2] bg-white border-b-[#D6D6D6] px-0'>
+            <div className='max-w-full flex items-center justify-start flex-row'>
+                <span className='w-full text-4xl font-bold text-center m-0 p-0 block max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-[#d69a2bc6]'>Food Finder</span>
+            </div>
+        </div>
+    );
+}
