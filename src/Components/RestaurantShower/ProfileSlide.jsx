9ecedbf6383d29791b2fd4f5ee66ae6c4806ce36
Afanc0 <gianromani.gr@gmail.com>
2024-03-13 18:33:24 -0400

    added white color background to fix visual bug

--- src/Components/RestaurantShower/ProfileSlide.jsx
+++ src/Components/RestaurantShower/ProfileSlide.jsx
@@ -28,23 +28,23 @@ function ProfileSlide() {
     }, [userid]);
 
     return (
-        <div className='w-[360px] shadow-md shadow-slate-300 rounded-md absolute right-0 z-[400]'>
+        <div className='w-[360px] shadow-md bg-white shadow-slate-300 rounded-md absolute right-0 z-[400]'>
             <div className='max-w-full'>
                 <div className='max-w-full px-4 py-2 flex flex-col gap-3'>
-                    <div className='max-w-full flex items-center gap-4 py-4 px-3 rounded-md shadow-lg shadow-slate-300'>
+                    <div className='max-w-full bg-white flex items-center gap-4 py-4 px-3 rounded-md shadow-lg shadow-slate-300'>
                         <div className='max-w-full flex items-center justify-center flex-row'>
                             <div className="w-[36px] h-[36px] rounded-full flex items-center justify-center bg-gray-300">
                                 <FontAwesomeIcon icon={faUser} />
                             </div>
                         </div>
                         <div className='max-w-full'>
-                            <span className='text-[1.0625rem] leading-[1.1765] font-semibold tracking-normal text-black whitespace-nowrap overflow-hidden overflow-ellipsis box-border'>
+                            <span className='text-[1.0625rem] leading-[1.1765] font-semibold tracking-normal text-black whitespace-nowrap overflow-hidden overflow-ellipsis box-border select-none'>
                                 {name}
                             </span>
                         </div>
                     </div>
                     <div className='max-w-full mt-2'>
-                        <div className='max-w-full rounded-md hover:bg-zinc-100 cursor-pointer'>
+                        <div className='max-w-full rounded-md bg-white hover:bg-zinc-100 cursor-pointer'>
                             <div className='max-w-full flex items-center gap-4 p-2'>
                                 <div className='max-w-full flex items-center justify-center flex-row'>
                                     <div className="w-[36px] h-[36px] rounded-full flex items-center justify-center bg-gray-300">
@@ -52,13 +52,13 @@ function ProfileSlide() {
                                     </div>
                                 </div>
                                 <div className='max-w-full'>
-                                    <span className='text-[.9375rem] leading-[1.3333] font-medium block text-black text-left overflow-hidden overflow-ellipsis whitespace-nowrap box-border'>
+                                    <span className='text-[.9375rem] leading-[1.3333] font-medium block text-black text-left overflow-hidden overflow-ellipsis whitespace-nowrap box-border select-none'>
                                         Settings
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className='max-w-full rounded-md hover:bg-zinc-100 cursor-pointer'>
+                        <div className='max-w-full rounded-md bg-white hover:bg-zinc-100 cursor-pointer'>
                             <div className='max-w-full flex items-center gap-4 p-2'>
                                 <div className='max-w-full flex items-center justify-center flex-row'>
                                     <div className="w-[36px] h-[36px] rounded-full flex items-center justify-center bg-gray-300">
@@ -66,13 +66,13 @@ function ProfileSlide() {
                                     </div>
                                 </div>
                                 <div className='max-w-full'>
-                                    <span className='text-[.9375rem] leading-[1.3333] font-medium block text-black text-left overflow-hidden overflow-ellipsis whitespace-nowrap box-border'>
+                                    <span className='text-[.9375rem] leading-[1.3333] font-medium block text-black text-left overflow-hidden overflow-ellipsis whitespace-nowrap box-border select-none'>
                                         Switch to vendor
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className='max-w-full rounded-md hover:bg-zinc-100 cursor-pointer' onClick={handleLogout}>
+                        <div className='max-w-full rounded-md bg-white hover:bg-zinc-100 cursor-pointer' onClick={handleLogout}>
                             <div className='max-w-full flex items-center gap-4 p-2'>
                                 <div className='max-w-full flex items-center justify-center flex-row'>
                                     <div className="w-[36px] h-[36px] rounded-full flex items-center justify-center bg-gray-300">
@@ -80,7 +80,7 @@ function ProfileSlide() {
                                     </div>
                                 </div>
                                 <div className='max-w-full'>
-                                    <span className='text-[.9375rem] leading-[1.3333] font-medium  block text-black text-left overflow-hidden overflow-ellipsis whitespace-nowrap box-border'>
+                                    <span className='text-[.9375rem] leading-[1.3333] font-medium  block text-black text-left overflow-hidden overflow-ellipsis whitespace-nowrap box-border select-none'>
                                         Log Out
                                     </span>
                                 </div>
