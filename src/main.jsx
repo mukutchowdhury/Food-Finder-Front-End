48d254858e71266da67036028ed5fba8ce8cdeb6
Mukut <mc8100@nyu.edu>
2024-03-17 15:33:04 -0400

    Added style for VendorForm

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import HomeView from './views/home'
-import LoginForm from './views/LoginForm.jsx'
-import AuthForm from './views/AuthForm.jsx'
 import App from './App.jsx';
-import RestaurantView from './views/restaurantView.jsx';
+import AccessDenied from './views/Access-Denied.jsx';
+import AuthForm from './views/AuthForm.jsx';
+import LoginForm from './views/LoginForm.jsx';
 import CategoryAll from './views/categoryAll.jsx';
 import Settings from './views/developer-settings.jsx';
-import AccessDenied from './views/Access-Denied.jsx';
+import HomeView from './views/home';
+import RestaurantView from './views/restaurantView.jsx';
 
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import './index.css';
@@ -23,6 +23,7 @@ const router = createBrowserRouter(createRoutesFromElements(
       <Route path="/dev-settings/*" element={<Settings />} />
       <Route path='/restaurant/:id' element={<RestaurantView/>}/>
       <Route path='access-denied' element={<AccessDenied/>}/>
+      {/* <Route path = 'vendorform' element = {<VendorForm/>}/> */}
   </Route>
 ));
 
--- src/views/VendorForm.jsx
+++ src/views/VendorForm.jsx
@@ -1,83 +1,37 @@
 import React, { useState } from 'react';
-import '../styling/VendorForm.css';
+import './vendorForm.css';
 
-function VendorFormView() {
-    // forms
-    const [formdata, setFormData] = useState({
-        rest_id: '',
-        rest_name: '',
-        rest_address: '',
-        rest_zipcode: '',
-        rest_owner_id: '',
-        rest_image: '',
-        phone_num:'',
-        cuisine:''
+const VendorForm = () => {
+    const [showForm, setShowForm] = useState(false);
 
-    });
-
-    const handleInput = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formdata,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formdata);
-        setFormData({
-            vendor_id: '',
-            vendor_name: '',
-            vendor_address: '',
-            vendor_zipcode: '',
-            vendor_owner_id: '',
-            vendor_image: '',
-            phone_num: '',
-            cuisine: ''
-        });
+    const handleEditClick = () => {
+        setShowForm(true);
     };
 
     return (
         <div className="vendor-form-container">
-            <h2>Vendor Registration</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Vendor ID:
-                    <input type="text" name="vendor_id" value={formdata.vendor_id} onChange={handleInput} />
-                </label>
-                <label>
-                    Vendor Name:
-                    <input type="text" name="rest_name" value={formdata.vendor_name} onChange={handleInput} />
-                </label>
-                <label>
-                    Address:
-                    <input type="text" name="rest_address" value={formdata.vendor_address} onChange={handleInput} />
-                </label>
-                <label>
-                    Zip Code:
-                    <input type="text" name="rest_zipcode" value={formdata.vendor_zipcode} onChange={handleInput} />
-                </label>
-                <label>
-                    Owner ID:
-                    <input type="text" name="rest_owner_id" value={formdata.vendor_owner_id} onChange={handleInput} />
-                </label>
-                <label>
-                    Image:
-                    <input type="text" name="rest_image" value={formdata.vendor_image} onChange={handleInput} />
-                </label>
-                <label>
-                    Phone Number:
-                    <input type="text" name="phone_num" value={formdata.phone_num} onChange={handleInput} />
-                </label>
-                <label>
-                    Cuisine:
-                    <input type="text" name="cuisine" value={formdata.cuisine} onChange={handleInput} />
-                </label>
-                <button type="submit">Vendor Registration</button>
-            </form>
+            <h2>Restaurant Information</h2>
+            <div className="restaurant-box">
+                <p>Restaurant Name: Restaurant Name 1</p>
+                <p>Address: 123 Main St</p>
+                <p>Phone: (123) 456-7890</p>
+                <button className="edit-btn" onClick={handleEditClick}>Edit</button>
+            </div>
+
+            {showForm && (
+                <form className="edit-form">
+                    <h2>Edit Restaurant</h2>
+                    <label htmlFor="editName">Name:</label><br />
+                    <input type="text" id="editName" name="editName" /><br />
+                    <label htmlFor="editAddress">Address:</label><br />
+                    <input type="text" id="editAddress" name="editAddress" /><br />
+                    <label htmlFor="editPhone">Phone:</label><br />
+                    <input type="text" id="editPhone" name="editPhone" /><br /><br />
+                    <button type="submit">Save Changes</button>
+                </form>
+            )}
         </div>
     );
-}
+};
 
-export default VendorFormView;
+export default VendorForm;
