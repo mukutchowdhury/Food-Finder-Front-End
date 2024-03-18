74d59c74971d97276ac88b1b0de1db5a0e2edbd6
jw6680 <jw6680@nyu.edu>
2024-03-17 22:06:23 -0400

    modified menus page

--- src/views/menuView.jsx
+++ src/views/menuView.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import HeaderLogo from '../Components/HeaderLogo/HeaderLogo';
+import Navigation from '../Components/Navigation';
+import Taco from "../assets/Terrific-tacos.jpeg"
+import Burger from "../assets/burger.jpeg"
 
-function MenuView() {
+
+const MenuView = () => {
+  const menuItems = [
+    { id: 1, name: 'Tacos', price: 10, image: Taco },
+    { id: 2, name: 'Burger', price: 15, image: Burger },
+  ];
+
   return (
     <div className="menu-container">
-      <h1 className="menu-title">Menu</h1>
+        <div className="header">
+            <HeaderLogo />
+            <Navigation />
+        </div>
+      <h1 className="menu-title">Terric Taco Menu</h1>
       <div className="menu-items">
-        {/* menu items goes here */}
-        <div className="menu-item">
-          <h3>Item 1</h3>
-          <p>Description of item 1</p>
-          <span>$10</span>
-        </div>
-        <div className="menu-item">
-          <h3>Item 2</h3>
-          <p>Description of item 2</p>
-          <span>$15</span>
-        </div>
+        {menuItems.map((item) => (
+          <div key={item.id} className="menu-item">
+            <img src={item.image} alt={item.name} className="item-image" />
+            <h3>{item.name}</h3>
+            <p>${item.price}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default MenuView;
