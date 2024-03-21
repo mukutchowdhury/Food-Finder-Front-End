70c3343888f929f18e5c61ed481be08ac66248d5
afanc0 <gianromani.gr@gmail.com>
2024-03-20 20:50:39 -0400

    changed path to path on click

--- src/views/Access-Denied.jsx
+++ src/views/Access-Denied.jsx
@@ -6,7 +6,7 @@ function AccessDenied() {
             <h1 className="text-4xl font-bold mb-4">Access Denied</h1>
             <p className="text-gray-600 mb-8">You do not have permission to access this page.</p>
             <button className="bg-[#d69a2bc6] hover:bg-[#d69a2be8] text-white font-bold py-2 px-4 rounded">
-                <a href="/">Return to Home</a>
+                <a href="/home">Return to Home</a>
             </button>
         </div>
         </>
