94fe6c72a75c68e666a2df4f8ae5533d5713b4fc
Masrur Kabir <freshcake189@gmail.com>
2024-03-31 23:58:40 -0400

    uncomment line

--- src/views/restaurantHoursView.jsx
+++ src/views/restaurantHoursView.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-//import '../styling/restaurantHourStyle.css';
+import '../styling/restaurantHourStyle.css';
 
 function RestaurantHourView() {
     // forms
