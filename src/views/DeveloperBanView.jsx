32512535376609ba953fa14f64c92cd35397de14
Masrur Kabir <freshcake189@gmail.com>
2024-04-21 23:49:28 -0400

    Developer Ban View

---
+++ src/views/DeveloperBanView.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+//import '../styling/DeveloperBanViewStyle.css';
+
+function DeveloperBanView() {
+    // forms
+    const [formdata, setFormData] = useState({
+        user_id: '',
+
+    });
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formdata,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formdata);
+        setFormData({
+            user_id: '',
+        });
+    };
+
+    return (
+        <div className="deletion-form-container">
+            <h2>Ban User Permanently</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    User ID:
+                    <input type="text" name="user_id" value={formdata.user_id} onChange={handleInput} />
+                </label>
+                <button type="submit">Ban User Permanently</button>
+            </form>
+        </div>
+    );
+}
+
+export default DeveloperBanView;
