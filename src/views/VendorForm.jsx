f937ed91faf54d143c0f2550f4c5cccc05cc1bc3
Masrur Kabir <freshcake189@gmail.com>
2024-04-30 09:47:34 -0400

    Update vendor form with restaurant deletion capabilities

--- src/views/VendorForm.jsx
+++ src/views/VendorForm.jsx
@@ -8,6 +8,7 @@ const VendorForm = () => {
     const [showForm, setShowForm] = useState(false);
     const [editingRestaurant, setEditingRestaurant] = useState(null);
     const [formData, setFormData] = useState([]);
+    const [restaurant_id, setRestaurantId] = useState('');
 
     const userId = localStorage.getItem("userid");
 
@@ -38,6 +39,18 @@ const VendorForm = () => {
         console.log('Creating new restaurant');
     };
 
+    const handleDeleteRestaurant = async () => {
+        try {
+            await axios.delete(`${BACKEND_URL}/restaurants/${restaurant_id}`);
+            const response = await axios.get(`${BACKEND_URL}/restaurant/${restaurant_id}`);
+            setFormData(response.data.review); 
+            setRestaurantId('');
+        } catch (error) {
+            setError('Could not delete the restaurant');
+        }
+        console.log('Deleting an existing restaurant');
+    };
+
     const arrayifyRestaurantsObject = () => {
         return Object.keys(formData);
     }
@@ -54,6 +67,20 @@ const VendorForm = () => {
                     <div className="options-box" onClick={handleCreateRestaurant}>
                         <h4>Create Restaurant</h4>
                     </div>
+                </div>
+                <div className="options-boxes">
+                    <div className="options-box" onClick={handleDeleteRestaurant}>
+                        <h4>Delete Restaurant</h4>
+                    </div>
+                    <div className="delete-review-container">
+                    <input 
+                        type="text"
+                        placeholder="Enter Restaurant ID "
+                        value={restaurant_id}
+                        onChange={e => setRestaurantId(e.target.value)}
+                    />
+                    <button onClick={handleDeleteRestaurant}>Confirm</button>
+                </div>
                 </div>
             </div>
 
